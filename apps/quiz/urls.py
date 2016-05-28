a735725b8c88a885a62febc55bab33676ecba823
Alyson Takai <shtakai@gmail.com>
2016-05-28 11:51:55 -0700

    add show returns id

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.show, name='show'),
 ]
--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'quiz/index.html')
 
+
+def show(request, question_id):
+    return HttpResponse(
+        "You are looking at question number {}.".format(question_id))
