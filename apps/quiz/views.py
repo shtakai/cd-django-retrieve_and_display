41c1ef31d3cbd33078f0078a8560d2daa07c2a00
Alyson Takai <shtakai@gmail.com>
2016-05-28 11:56:44 -0700

    added HttpResponseNotFound

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -11,4 +11,6 @@ def show(request, question_id):
         return HttpResponse(
             "You are looking at question number {}.".format(question_id))
     else:
-        raise Http404
+        return HttpResponseNotFound(
+            'Page not found'
+        )
