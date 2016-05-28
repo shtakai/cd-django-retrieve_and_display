bae0d41bde2193e3a99e441f6344d0d4c18b6989
Alyson Takai <shtakai@gmail.com>
2016-05-28 12:54:24 -0700

    passing data

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>\d+)/$', views.show, name='show'),
 ]
--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -3,14 +3,27 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'quiz/index.html')
-
-
-def show(request, question_id):
-    if int(question_id) == 1:
-        return HttpResponse(
-            "You are looking at question number {}.".format(question_id))
-    else:
-        return HttpResponseNotFound(
-            'Page not found'
-        )
+    context = {
+        'questions': [
+            {
+                'id': 1,
+                'content':
+                'Why is there a light in the fridge and not in the freezer?'
+            },
+            {
+                'id': 2,
+                'content': 'Why don\'t sheep shrink when it rains?'
+            },
+            {
+                'id': 3,
+                'content':
+                'Why are the called apartments when they are all together?'
+            },
+            {
+                'id': 4,
+                'content':
+                    'Why are cigarettes sold where smoking is prohibited?'
+            },
+        ]
+    }
+    return render(request, 'quiz/index.html', context)
