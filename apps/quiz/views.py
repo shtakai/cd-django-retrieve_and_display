a80de94a23f340d15ada7652f137dfc3fee9677b
Alyson Takai <shtakai@gmail.com>
2016-05-28 11:42:01 -0700

    add templete

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello World!")
-
+    return render(request, 'quiz/index.html')
 
