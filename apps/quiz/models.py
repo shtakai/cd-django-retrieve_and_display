db1f20b1a1c31a12d75bac97780427033223d488
Alyson Takai <shtakai@gmail.com>
2016-05-28 13:50:47 -0700

    add model classes

--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -16,3 +16,38 @@ class Choice(models.Model):
     class Meta:
         db_table = 'choices'
 
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'musicians'
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    release_date = models.IntegerField()
+    num_stars = models.IntegerField()
+    musician = models.ForeignKey(Musician)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'albums'
+
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
